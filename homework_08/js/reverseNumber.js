c389d62f0bb0452045a81336dfd6ff8217514de4
Mykhailo Panas <lv211184pmi@gmail.com>
2018-02-17 07:08:14 +0200

    last function added

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,3 +1,13 @@
-function reverseNumber(){
-    
+function reverseNumber(arg){
+    let reverseArg = arg.toString();
+    reverseArg = reverseArg.split('');
+    reverseArg = reverseArg.reverse();
+    if(reverseArg[reverseArg.length-1]==='-'){
+        reverseArg.pop();
+        reverseArg = Number(reverseArg.join(''));
+        return -Math.abs(reverseArg);
+    } else {
+        reverseArg = reverseArg.join('');
+        return Number(reverseArg);
+    }
 }
