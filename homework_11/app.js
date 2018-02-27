c080c631ff66afa0c486e3b6335d1b893fa5f389
Mykhailo Panas <lv211184pmi@gmail.com>
2018-02-27 15:21:07 +0200

    DOM tree building function was improved

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,5 +1,39 @@
 var rootNode = document.getElementById("root");
 
-// Your code goes here
+let createNode = (iconType, title) => {
+    let liEl = document.createElement('li');
+    liEl.className = "show";
+    let iEl = document.createElement('i');
+    iEl.className = "material-icons";
+    iEl.innerHTML = iconType;
+    let pEl = document.createElement('p');
+    pEl.innerHTML = title;
+    liEl.appendChild(iEl);
+    liEl.appendChild(pEl);
+    return liEl;
+}
 
-rootNode.appendChild(/* Append your TreeView node*/);
+let TreeView = (inArr) => {
+    let ulNode = document.createElement("ul");
+    inArr.forEach(element => {
+        let liInner;
+        if(element.folder) {
+            liInner = createNode("folder", element.title);
+        } else {
+            liInner = createNode("library_books", element.title);
+        }
+        if(element.children) {
+            let ulChild = document.createElement("ul");
+            ulChild.appendChild(convert(element.children));
+            liInner.appendChild(ulChild);
+        }
+        ulNode.appendChild(liInner);
+    });
+    return ulNode;
+}
+
+rootNode.appendChild(TreeView(structure));
+
+
+
+
